79b571a8638dfc45e2a2fb6dde278314d50852eb
Shreyas Mudakavi <shreyasmudak@gmail.com>
2023-09-13 18:20:11 +0530

    backend url change

--- src/utils/axiosUtil.js
+++ src/utils/axiosUtil.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // baseURL: "http://localhost:5000",
-  baseURL: "https://car-rental-backend.adaptable.app",
+  baseURL: "https://api.carrentalpalmerstonnorth.co.nz",
+  // baseURL: "https://car-rental-backend.adaptable.app",
 });
 
 export default axiosInstance;
